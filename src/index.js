ce1a8c2fa6e6078c8415d9c4f68abae084f1cef1
ihor <onyshchukihor11@gmail.com>
2020-07-28 12:06:22 +0300

    observable object & array

--- src/index.js
+++ src/index.js
@@ -4,25 +4,30 @@ import DevTools from 'mobx-react-devtools';
 import { observer } from 'mobx-react';
 
 import * as serviceWorker from './serviceWorker';
-import { observable, computed } from 'mobx';
+import { observable, computed, extendObservable } from 'mobx';
 
-const nickName = new class UserNickName {
-  @observable firstName = "Ihor";
-  @observable age = 23;
+const nickName = observable({
+  firstName: 'Ihor',
+  age: 23,
 
-  @computed get nickName() {
+  get nickName() {
     console.log('generate nickName');
     return `${this.firstName}${this.age}`
+  },
+
+  increment() {
+    this.age++
+  },
+
+  decrement() {
+    this.age--
   }
-}
+});
 
-nickName.increment = function () {
-  this.age++
-}
-
-nickName.decrement = function () {
-  this.age--
-}
+const todos = observable([
+  { text: 'Learn React' },
+  { text: 'Learn Mobx' }
+]);
 
 @observer class Counter extends Component {
 
@@ -31,18 +36,23 @@ nickName.decrement = function () {
 
 
   render() {
+    const { nickName, age } = this.props.store;
     return (
       <div className="App">
         <DevTools />
-        <h2>{this.props.store.nickName}</h2>
-        <h2>{this.props.store.age}</h2>
+        {/* <h2>{nickName}</h2>
+        <h2>{age}</h2>
         <button onClick={this.handleDecrement}>-1</button>
-        <button onClick={this.handleIncrement}>+1</button>
+        <button onClick={this.handleIncrement}>+1</button> */}
+
+        <ul>
+          {todos.map(({ text }) => <li key={text}>{text}</li>)}
+        </ul>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Counter store={nickName} />, document.getElementById('root'));
+ReactDOM.render(<Counter store={todos} />, document.getElementById('root'));
 
 serviceWorker.unregister();
